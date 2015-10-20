c4468f7663c46f28b92f74c9a13c24c2774f6123
Hamza Abbad <hamza.abbad@gmail.com>
2015-10-20 22:09:58 +0100

    Améliorer la détéction du type de paramètre
    Tester d'abord si le paramètre du programme est un fichier, sinon, supposer qu'il est un entier.

--- src/miniproject1/Main.java
+++ src/miniproject1/Main.java
@@ -1,6 +1,7 @@
 package miniproject1;
 
 import javax.swing.SwingUtilities;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,45 +10,51 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
         try {
-            FileReader fileReader;
-            try {
-                if (args.length != 1) throw new IllegalArgumentException();
-                final long number = Long.parseLong(args[0]);
-                if (number < 0) throw new NumberFormatException();
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        AlgorithmsProgressWindow progressWindow = new AlgorithmsProgressWindow();
-                        progressWindow.start(number);
+            if (args.length != 1) throw new IllegalArgumentException();
+            else {
+                try {
+                    File numbersFile = new File(args[0]);
+                    if (numbersFile.exists()) {
+                        FileReader fileReader = new FileReader(numbersFile);
+                        final ArrayList<Long> numbers = new ArrayList<>();
+                        Scanner numbersReader = new Scanner(fileReader);
+                        while (numbersReader.hasNextLong()) {
+                            long number = numbersReader.nextLong();
+                            if (number < 0) throw new NumberFormatException();
+                            numbers.add(number);
+                        }
+                        fileReader.close();
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                AlgorithmsProgressWindow progressWindow = new AlgorithmsProgressWindow();
+                                progressWindow.start(numbers);
+                            }
+                        });
+                    } else {
+                        try {
+                            final long number = Long.parseLong(args[0]);
+                            if (number < 0) throw new NumberFormatException();
+                            SwingUtilities.invokeLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    AlgorithmsProgressWindow progressWindow = new AlgorithmsProgressWindow();
+                                    progressWindow.start(number);
+                                }
+                            });
+                        } catch(NumberFormatException ex){
+                            System.err.println("Les entiers  doivent êtrepositifs !");
+                            System.exit(3);
+                        }
                     }
-                });
-            } catch (NumberFormatException ex) {
-                fileReader = new FileReader(args[0]);
-                final ArrayList<Long> numbers = new ArrayList<>();
-                Scanner numbersReader = new Scanner(fileReader);
-                while(numbersReader.hasNextLong()){
-                    long number = numbersReader.nextLong();
-                    if(number < 0) throw new NumberFormatException();
-                    numbers.add(number);
+                } catch(IOException ex){
+                    System.err.printf("Erreur lors de l'utilisation ddu fichier %s !%n", args[0]);
+                    System.exit(2);
                 }
-                fileReader.close();
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        AlgorithmsProgressWindow progressWindow = new AlgorithmsProgressWindow();
-                        progressWindow.start(numbers);
-                    }
-                });
             }
-        } catch(NumberFormatException ex){
-            System.err.printf("Le fichier %s doit contenir que des entiers positifs !%n", args[0]);
-            System.exit(3);
         } catch(IllegalArgumentException ex){
             System.err.printf("Usage : java %s (<entier_positif> | <fichier_entiers_positifs>)%n", "Main");
             System.exit(1);
-        } catch(IOException ex){
-            System.err.printf("Le fichier %s n'existe pas !%n", args[0]);
-            System.exit(2);
         }
 	}
 }
