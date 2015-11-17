69c1a01e979bfe3e571e82df2823d11155904384
Hamza Abbad <hamza.abbad@gmail.com>
2015-11-17 22:50:35 +0100

    Ajouter la possibilité de spécifier plusieurs valeurs
    Le programme maintenant accepte plusieurs entiers pour spécifier le minimum du tableau, le maximum et les valeurs intermédiaires dont il faut sauvegarder le temps d'exécution.

--- src/miniproject2/Main.java
+++ src/miniproject2/Main.java
@@ -6,22 +6,31 @@ public class Main {
 	public static void main(String[] args){
 		int[] numbersTable;
 		try {
-			if (args.length != 3) throw new IllegalArgumentException();
-			int size = Integer.parseInt(args[1]);
-            if(size < 1) throw new NumberFormatException();
+			if (args.length < 3) throw new IllegalArgumentException();
+			int min = Integer.parseInt(args[2]);
+            int max = Integer.parseInt(args[args.length-1]);
+            if (min > max) {
+                int x = min;
+                min = max;
+                max = x;
+            }
+            int[] checkValues = new int[args.length - 2];
+            for (int i = 0; i < checkValues.length; i++) checkValues[i] = Integer.parseInt(args[i+2]);
+            if (min == max) min = 1;
+            if(max < 1) throw new NumberFormatException();
 			String algorithmName = args[0].toLowerCase();
-			numbersTable = new int[size];
-            String testCase = args[2].toLowerCase();
+			numbersTable = new int[max - min + 1];
+            String testCase = args[1].toLowerCase();
             boolean inWorstCase = false;
             switch (testCase) {
                 case "meilleur":
-                    for (int i = 0; i < size; i++) {
-                        numbersTable[i] = i + 1;
+                    for (int i = 0; i < numbersTable.length; i++) {
+                        numbersTable[i] = min++;
                     }
                     break;
                 case "pire":
-                    for (int i = 0; i < size; i++) {
-                        numbersTable[size - i - 1] = i + 1;
+                    for (int i = 0; i < numbersTable.length; i++) {
+                        numbersTable[i] = max--;
                     }
                     inWorstCase = true;
                     break;
@@ -32,31 +41,31 @@ public class Main {
             AlgorithmsProgressWindow progressWindow = new AlgorithmsProgressWindow();
             switch (algorithmName) {
                 case "sélection":
-                    progressWindow.start(numbersTable, Algorithm.Selection, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.Selection, inWorstCase, checkValues);
                     break;
                 case "insertion":
-                    progressWindow.start(numbersTable, Algorithm.Insertion, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.Insertion, inWorstCase, checkValues);
                     break;
                 case "bulles":
-                    progressWindow.start(numbersTable, Algorithm.Bubble, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.Bubble, inWorstCase, checkValues);
                     break;
                 case "rapide":
-                    progressWindow.start(numbersTable, Algorithm.Rapid, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.Rapid, inWorstCase, checkValues);
                     break;
                 case "fusion":
-                    progressWindow.start(numbersTable, Algorithm.Merge, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.Merge, inWorstCase, checkValues);
                     break;
                 case "tas":
-                    progressWindow.start(numbersTable, Algorithm.Heap, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.Heap, inWorstCase, checkValues);
                     break;
                 case "rapide2":
-                    progressWindow.start(numbersTable, Algorithm.RapidIterative, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.RapidIterative, inWorstCase, checkValues);
                     break;
                 case "fusion2":
-                    progressWindow.start(numbersTable, Algorithm.MergeIterative, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.MergeIterative, inWorstCase, checkValues);
                     break;
                 case "tas2":
-                    progressWindow.start(numbersTable, Algorithm.HeapIterative, inWorstCase);
+                    progressWindow.start(numbersTable, Algorithm.HeapIterative, inWorstCase, checkValues);
                     break;
                 default:
                     System.err.println("Algorithme inconnue : "+algorithmName);
@@ -68,9 +77,11 @@ public class Main {
             System.exit(2);
 		}
 		catch(IllegalArgumentException ex){
-            System.err.printf("Usage : %s <algorithme> <taille_tableau> (pire|meilleur)%n" +
-                            " <algorithme> : bulles|sélection|insertion|fusion|rapide|tas|fusion2|rapide2|tas2%n",
-                            "miniproject2.Main");
+            System.err.printf("Usage : %s <algorithme> (pire|meilleur) <valeur> [<valeur_i>, ...]%n" +
+                            " <algorithme> : bulles|sélection|insertion|fusion|rapide|tas|fusion2|rapide2%n" +
+                            " <valeur> : La valeur minimale dans le tableau (ou maximale s'il n'a pas de <valeur_i>)%n" +
+                            " <valeur_i> : Une ou plusieurs valeurs intermédiaires quand il faut enregistrer le temps d'exécution%n" +
+                            "              La dernière valeur est la valeur maximale du tableau",                            "miniproject2.Main");
             System.exit(1);
 		}
 	}
